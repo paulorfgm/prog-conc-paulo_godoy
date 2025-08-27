d9f2f935f09d2d7c48010403c936e5f94593e07c
paulorfgm <paulorfgm@dcc.ufrj.br>
2025-08-26 22:31:47 -0300

    Entrega 4

---
+++ [ENTREGA] LAB3/scripts/create_vetores.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcc ../src/prod_int_seq.c -o ../bin/programa_sequencial.exe -Wall && ../bin/programa_sequencial.exe 1000 ../data/teste1.bin
+gcc ../src/prod_int_seq.c -o ../bin/programa_sequencial.exe -Wall && ../bin/programa_sequencial.exe 10000 ../data/teste2.bin
+gcc ../src/prod_int_seq.c -o ../bin/programa_sequencial.exe -Wall && ../bin/programa_sequencial.exe 100000 ../data/teste3.bin
---
+++ [ENTREGA] LAB3/scripts/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc ../src/prod_int_conc.c -o ../bin/programa_concorrente.exe -Wall && ../bin/programa_concorrente.exe 4 ../data/teste3.bin
